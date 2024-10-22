c0acf635b72e8ad85b46a509f4bd49c7d19b8f7b
Rok Garbas <rok@flox.dev>
2024-10-22 16:42:23 +0200

    use NGINX_PORT variable in test

--- nginx/test.sh
+++ nginx/test.sh
@@ -1,6 +1,6 @@
 set -euxo pipefail
 
-if curl -s http://localhost:8080 | grep -q "<title>Hello Flox!</title>"; then
+if curl -s http://localhost:$NGINX_PORT| grep -q "<title>Hello Flox!</title>"; then
     echo ">>> Nginx serving correctly!"
 else
     echo "ERROR: Nginx not working correctly!"
