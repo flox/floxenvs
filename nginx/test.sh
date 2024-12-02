1d5890d0ec96fdcb3fb29f0e1ebb6308b659bc29
flox automations <73946670+floxbot@users.noreply.github.com>
2024-12-02 17:22:34 +0100

    chore: Update manifest of `nginx` flox environment (#141)
    This PR was automatically created by [Update
    workflow](https://github.com/flox/floxenvs/actions/workflows/update.yml).
    
    ---------
    
    Co-authored-by: FloxBot <bot@flox.dev>
    Co-authored-by: Rok Garbas <rok@flox.dev>

--- nginx/test.sh
+++ nginx/test.sh
@@ -1,8 +1,29 @@
 set -euxo pipefail
 
-if curl -s http://localhost:$NGINX_PORT| grep -q "<title>Hello Flox!</title>"; then
-    echo ">>> Nginx serving correctly!"
-else
-    echo "ERROR: Nginx not working correctly!"
-    exit 1
+MAX_ATTEMPTS=10
+while [ $MAX_ATTEMPTS -gt 0 ]; do
+  set +e
+  STATUS=$(curl -s http://localhost:$NGINX_PORT| grep '<title>Hello Flox!</title>')
+  set -e
+  if [ "$STATUS" == "    <title>Hello Flox!</title>" ]; then
+    break
+  fi
+  echo -n ".."
+  sleep 1
+  MAX_ATTEMPTS=$((MAX_ATTEMPTS - 1))
+done
+
+if [ $MAX_ATTEMPTS -eq 0 ]; then
+  echo ""
+  echo "❌ Error: Nginx didn't come up in time."
+  exit 1
 fi
+
+echo ""
+echo "✅ Nginx serving correctly!"
+
+echo ">>> flox services status"
+flox services status
+
+echo ">>> flox services logs nginx"
+flox services logs nginx
