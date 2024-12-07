4329eacd9f2728a7603f2c69deaa16d5c1e02da4
Ross Turk <ross@floxdev.com>
2024-12-06 19:45:30 -0500

    Localstack environment
    Signed-off-by: Ross Turk <ross@floxdev.com>

---
+++ localstack/test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+command_exists() {
+  if ! command -v $1 2>&1 >/dev/null; then
+    echo "Error: '$1' command could not be found."
+    return 1
+  fi
+  echo ">>> '$1' command exists"
+}
+
+command_exists localstack
+command_exists kubectl
+command_exists gum
+
+localstack status services
