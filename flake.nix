75e1d05b3f0437d75c56b2113c6b7dac12ec2462
Zach Mitchell <zmitchell@fastmail.com>
2025-02-19 18:03:17 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  inputs.flox.url = "github:flox/flox/refs/tags/v1.3.13";
+  inputs.flox.url = "github:flox/flox/refs/tags/v1.3.14-rc.1";
 
   outputs =
     {
