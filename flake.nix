cfa58d72d4918374ae1e818ebb11f042ff33ee44
Matthew Kenigsberg <matthew@flox.dev>
2026-01-29 12:57:19 -0700

    bump: 1.8.3 -> 1.9.0

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flox.url = "github:flox/flox/refs/tags/v1.8.3";
+  inputs.flox.url = "github:flox/flox/release-1.9.0";
 
   outputs =
     {
