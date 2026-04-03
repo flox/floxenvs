d3d48dd3c7e5e6d264605246d67a23e35847788b
Rok Garbas <rok@flox.dev>
2026-04-03 16:52:20 +0200

    feat: add localstack-demo environment

---
+++ localstack-demo/test.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+command_exists() {
+  if ! command -v "$1" >/dev/null 2>&1; then
+    echo "Error: '$1' command not found."
+    return 1
+  fi
+  echo ">>> '$1' command exists"
+}
+
+command_exists localstack
+command_exists aws
+command_exists kubectl
+command_exists gum
+
+echo -n ">>> Waiting for LocalStack to start .."
+MAX_ATTEMPTS=30
+while [ "$MAX_ATTEMPTS" -gt 0 ]; do
+  if localstack status | grep -q running 2>/dev/null; then
+    echo -e "\n>>> LocalStack STARTED SUCCESSFULLY\n"
+    break
+  fi
+  echo -n ".."
+  MAX_ATTEMPTS=$((MAX_ATTEMPTS - 1))
+  sleep 2
+done
+
+if [ "$MAX_ATTEMPTS" -eq 0 ]; then
+  echo -e "\nError: LocalStack not ready after 60 seconds"
+  exit 1
+fi
+
+echo ">>> flox services status"
+flox services status
+
+echo ">>> flox services logs localstack"
+flox services logs localstack
