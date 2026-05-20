e7ff47bec2f5a5c0317dd3f1127c2cc4b23376ea
Rok Garbas <rok@flox.dev>
2026-05-20 14:41:06 +0200

    feat(temporal-demo): add worker that registers greeting workflow

---
+++ temporal-demo/worker/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"flox.dev/temporal-demo/greeting"
+
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+)
+
+func main() {
+	c, err := client.Dial(client.Options{})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, "my-task-queue", worker.Options{})
+	w.RegisterWorkflow(greeting.SayHelloWorkflow)
+	w.RegisterActivity(greeting.Greet)
+
+	if err := w.Run(worker.InterruptCh()); err != nil {
+		log.Fatalln("Unable to start worker", err)
+	}
+}
